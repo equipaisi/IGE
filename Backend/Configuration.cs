adb9f29f822a1e012adf486383260ec933d1237d
Adolfo Silva <a9126@alunos.ipca.pt>
2016-11-10 16:35:32 +0000

    Add some initial database configuration code

--- Backend/Configuration.cs
+++ Backend/Configuration.cs
@@ -1,6 +1,38 @@
-﻿namespace Backend
+﻿using System;
+using System.Threading.Tasks;
+using Backend;
+using MySql.Data.MySqlClient;
+
+namespace Backend
 {
-    public abstract class Configuration
+    public interface IDbConnection: IDisposable
     {
+        void Initialize(string server, string database);
+        Task Open();
+        Task Close();
+    }
+
+    public class MySqlDbCon: IDbConnection
+    {
+        private MySqlConnection con;
+        private string uid;
+        private string password;
+
+        public MySqlDbCon(string server = "localhost", string database = "ige")
+        {
+            Initialize(server, database);
+        }
+
+        public void Initialize(string server = "localhost", string database = "ige")
+        {
+            uid = "username";
+            password = "password";
+            con = new MySqlConnection($"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};");
+        }
+
+        public Task Open() => con.OpenAsync();
+
+        public Task Close() => con.CloseAsync();
+        public void Dispose() => con.Dispose();
     }
 }
