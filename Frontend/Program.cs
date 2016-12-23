8cfc432a7a348ba14c2fb29696efad06f35d1c35
Adolfo Silva <a9126@alunos.ipca.pt>
2016-12-23 11:47:42 +0000

    Add Google Places API WCF service

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using Frontend.GoogleMapsService;
+using Frontend.GooglePlacesService;
 
 namespace Frontend
 {
@@ -20,6 +21,12 @@ namespace Frontend
             {
                 var place = new GoogleMapsClient().GetPlace("Rua José António Cruz, 4715-343, Braga");
                 Console.WriteLine($"Address=\"{place.FormattedAddress}\",Latitude={place.Latitude},Longitude={place.Longitude},PlaceId={place.PlaceId}");
+                var places = new GooglePlacesClient().GetPointsOfInterest(place.Latitude, place.Longitude, 500);
+                foreach (var e in places)
+                {
+                    Console.WriteLine($"Name={e.Name},Coord=({e.Latitude},{e.Longitude})");
+                    Console.WriteLine($"PlaceId={e.PlaceId},Name={e.Name},Coord=({e.Latitude},{e.Longitude}),Types=[{string.Join(",", e.Types)}]");
+                }
                 Application.Run(new FormLogin());
             }
             catch (Exception e)
---
+++ GooglePlaces/GooglePlaces.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.ServiceModel;
+using System.Text;
+
+namespace GooglePlaces
+{
+    public class GooglePlaces : IGooglePlaces
+    {
+        private const string ApiKey = "AIzaSyBPFsCVlF0_aZE46Tqutha3GVrqclwx7s0";
+
+        #region Public API
+
+        /// <summary>
+        /// Retorna uma lista de pontos de interesse, resultado de um Nearby Search.
+        /// </summary>
+        /// <param name="latitude">Latitude</param>
+        /// <param name="longitude">Longitude</param>
+        /// <param name="radius">Raio (em metros) a pesquisar</param>
+        /// <returns>Lista de <see cref="Place"/></returns>
+        public List<Place> GetPointsOfInterest(double latitude, double longitude, int radius)
+        {
+            if (radius <= 0 || radius > 50000)
+                throw new ArgumentOutOfRangeException("Radius must be a positive integer between 1 an 50 000");
+            var url = CreateRequestUrl(latitude, longitude, radius);
+            var root = Magic<RootObject>(url);
+            if (root.status != "OK") return null; // TODO: throw new Custom Exception
+            if (root.results.Count < 1) return null;
+
+            return root.results.Select(result => new Place
+            {
+                Name = result.name,
+                PlaceId = result.place_id,
+                Latitude = result.geometry.location.lat,
+                Longitude = result.geometry.location.lng,
+                Types = result.types
+            }).ToList();
+        }
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Retorna o URL para um Nearby Search. 
+        /// </summary>
+        /// <param name="latitude">Latitude</param>
+        /// <param name="longitude">Longitude</param>
+        /// <param name="radius">Raio (em metros) da pesquisa</param>
+        /// <returns></returns>
+        public string CreateRequestUrl(double latitude, double longitude, int radius)
+        {
+            var us = CultureInfo.CreateSpecificCulture("en-US");
+            var baseUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+            return $"{baseUrl}location={latitude.ToString(us)},{longitude.ToString(us)}&radius={radius}&key={ApiKey}";
+        }
+
+        private static T Magic<T>(string url)
+            => (T)new DataContractJsonSerializer(typeof(T)).ReadObject(GetResponse(CreateRequest(url)));
+
+        public static MemoryStream GetResponse(HttpWebRequest request)
+        {
+            using (var response = request.GetResponse() as HttpWebResponse)
+            {
+                // verificar erros na resposta
+                if (response != null && response.StatusCode != HttpStatusCode.OK)
+                    throw new Exception(string.Format(
+                        "Server error (HTTP {0}: {1}).",
+                        response.StatusCode,
+                        response.StatusDescription));
+
+                var result = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                return new MemoryStream(Encoding.Unicode.GetBytes(result)) { Position = 0 };
+            }
+        }
+
+        public static HttpWebRequest CreateRequest(string requestUrl) => WebRequest.Create(requestUrl) as HttpWebRequest;
+
+        #endregion
+    }
+}
